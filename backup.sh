d9593bf96dd6e9ee4ab97773c5b8b2c2de905f12
ChernoivanovAV <10735570+ChernoivanovAV@users.noreply.github.com>
2025-09-14 18:01:26 +0700

    - доработано описание - доработан скрипт резервного копирования - итоговая версия для проверки

--- backup.sh
+++ backup.sh
@@ -10,15 +10,41 @@ fi
 export $(grep -v '^#' "$ENV_FILE" | xargs)
 
 BACKUP_DIR="./backups"
-CURRENT_DATE=$(date +"%Y-%m-%d_%H%M%S")
-BACKUP_FILE="$BACKUP_DIR/backup_$CURRENT_DATE.sql"
 mkdir -p "$BACKUP_DIR"
 
-docker exec -t "$POSTGRES_CONTAINER" pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"
+if [ $# -eq 0 ]; then
+  CURRENT_DATE=$(date +"%Y-%m-%d_%H%M%S")
+  BACKUP_FILE="$BACKUP_DIR/backup_$CURRENT_DATE.sql"
+  docker exec -t "$POSTGRES_CONTAINER" pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"
 
-if [ $? -eq 0 ]; then
-    echo "Резервная копия успешно создана: $BACKUP_FILE"
+  if [ $? -eq 0 ]; then
+      echo "Резервная копия успешно создана: $BACKUP_FILE"
+  else
+      rm "$BACKUP_FILE"
+      echo "Ошибка при создании резервной копии!"
+  fi
 else
-    echo "Ошибка при создании резервной копии!"
-fi
+  RESTORE_FILE="$BACKUP_DIR/$1"
 
+  if [ ! -f "$RESTORE_FILE" ]; then
+    echo "!!! Файл $RESTORE_FILE не найден!"
+    exit 1
+  fi
+
+  echo "!!! ВНИМАНИЕ: База данных $POSTGRES_DB будет восстановлена из файла $RESTORE_FILE"
+  read -p "Продолжить? (y/n): " confirm
+  if [ "$confirm" != "y" ]; then
+    echo "Операция отменена."
+    exit 0
+  fi
+
+  docker exec -i "$POSTGRES_CONTAINER" psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+  docker exec -i "$POSTGRES_CONTAINER" psql -U "$POSTGRES_USER" "$POSTGRES_DB" < "$RESTORE_FILE"
+
+  if [ $? -eq 0 ]; then
+    echo "+++ База данных успешно восстановлена из $RESTORE_FILE"
+  else
+    echo "--- Ошибка при восстановлении базы данных"
+  fi
+
+fi
